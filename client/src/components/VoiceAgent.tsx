e4e92052731b23f4b0608f7cce4c47081daf9b50
Chhotuyadav <chhotuyadav2k1@gmail.com>
2026-03-31 00:42:31 +0530

    latest

--- client/src/components/VoiceAgent.tsx
+++ client/src/components/VoiceAgent.tsx
@@ -27,7 +27,7 @@ export default function VoiceAgent() {
       const backendUrl =
         process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:8000";
 
-      const res = await fetch(`${backendUrl}/token?identity=user`);
+      const res = await fetch(`${backendUrl}token?identity=user`);
       const { token } = await res.json();
 
       const device = new Device(token, { logLevel: "error" });
